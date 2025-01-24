903de77b90083dd67b7c13403ec2f5ffda3f58a7
farleybsd <farley.t.i@hotmail.com>
2025-01-24 14:22:52 -0300

    Strategy
    Strategy

---
+++ Design.Patterns.Ripiper/Comportamentais/Design.Patterns.Ripiper.Strategy/Program.cs
@@ -0,0 +1,6 @@
+﻿using Design.Patterns.Ripiper.Strategy.GOOD;
+
+var order = new Order(){Weight = 10,Destination = "Internacional"};
+order.ShippingStrategy = new InterNationallShippingStrategy();
+var cost = order.CalculateShippingCost();
+Console.WriteLine($"O Custo do Frete: {cost:C}");
---
+++ Design.Patterns.Ripiper/Comportamentais/DesignPatterns.Ripiper.memento/Program.cs
@@ -0,0 +1,17 @@
+﻿// Uso da boa pratica padrao Memento
+
+using DesignPatterns.Ripiper.memento.GOOD;
+
+var editor = new TextEditor();
+var history = new EditorHistory();
+
+history.SaveState(editor); // Salva o Estado Inicial
+editor.Write("Ola Mundo");
+history.SaveState(editor); // Salva o Estado Apos Escrever
+
+editor.Write("Mais Texto");
+Console.Write("Antes de Desfazer");
+history.Undo(editor); // Desfaz mais uma Acao
+
+Console.WriteLine("Depois de Desfazer");
+history.Undo(editor); // Desfaz mais uma Acao
